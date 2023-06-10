3723661f4480d5309e5850e770a9f434c1006cac
yankipls <59634927+yankipls-a11y@users.noreply.github.com>
2023-06-10 15:59:31 -0700

    Added initial rust files for testing

---
+++ main.rs
@@ -0,0 +1,13 @@
+
+    // Line comments which go to the end of the line.
+    /* Block comments which go to the closing delimiter. */
+
+
+
+// fn stands for function
+// main is a special function that gets executed immediately when you run rust programs
+// use 4 spaces rather than tabs, but it doesn't matter much
+
+fn main () {
+    println!("hello world");
+}
